bb26812993b4a7bce641b43d026c263de13bbd99
Richard_Tamarisk <3200226264@qq.com>
2024-03-24 15:47:00 +0800

    Init QuickInfo

--- widget.cpp
+++ widget.cpp
@@ -11,6 +11,21 @@ Widget::Widget(QWidget *parent)
     connect(ui->pushButton_Apply, SIGNAL(clicked(bool)), this, SLOT(apply_Pushed()));
 
     connect(ui->pushButton_Convert, SIGNAL(clicked(bool)), this, SLOT(convert_Pushed()));
+
+    //Init QuickInfo
+    quickInfo->videoIdx = 0;
+    quickInfo->width = 0;
+    quickInfo->height = 0;
+    quickInfo->colorSpace = "";
+    quickInfo->videoCodec= "";
+    quickInfo->videoBitRate = 0;
+    quickInfo->frameRate = 0;
+    quickInfo->audioIdx = 0;
+    quickInfo->audioCodec = "";
+    quickInfo->audioBitRate = 0;
+    quickInfo->channels = 0;
+    quickInfo->sampleFmt = "";
+    quickInfo->sampleRate = 0;
 }
 
 void Widget::apply_Pushed()

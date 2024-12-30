4e371e737c6821e93b193631a340b93901e52c00
JackLau1222 <2366536135@qq.com>
2024-12-30 11:48:49 +0800

    add function that can set the codec and bit rate of encoder

--- src/transcoder_bmf.cpp
+++ src/transcoder_bmf.cpp
@@ -8,6 +8,7 @@ TranscoderBMF::TranscoderBMF(ProcessParameter *processParameter,
 }
 
 bool TranscoderBMF::prepare_info(std::string input_path, std::string output_path) {
+    // decoder init
     if (encodeParameter->get_Video_Codec_Name() == "") {
         copyVideo = true;
     } else {
@@ -20,32 +21,58 @@ bool TranscoderBMF::prepare_info(std::string input_path, std::string output_path
         copyAudio = false;
     }
 
-    nlohmann::json video_codec = {
+    nlohmann::json de_video_codec = {
         "video_codec", ""
     };
-    nlohmann::json audio_codec = {
+    nlohmann::json de_audio_codec = {
         "audio_codec", ""
     };
 
     if (copyVideo) {
-        video_codec = {
+        de_video_codec = {
             "video_codec", "copy"
         };
     }
     if (copyAudio) {
-        audio_codec = {
+        de_audio_codec = {
             "audio_codec", "copy"
         };
     }
 
     decoder_para = {
         {"input_path", input_path},
-        video_codec,
-        audio_codec,
+        de_video_codec,
+        de_audio_codec,
+    };
+
+    // encoder init
+    nlohmann::json en_video_codec = {
+        "codec", encodeParameter->get_Video_Codec_Name()
+    };
+    nlohmann::json en_audio_codec = {
+        "codec", encodeParameter->get_Audio_Codec_Name()
+    };
+    nlohmann::json en_video_bitrate = {
+        "bit_rate", encodeParameter->get_Video_Bit_Rate()
+    };
+    nlohmann::json en_audio_bitrate = {
+        "bit_rate", encodeParameter->get_Audio_Bit_Rate()
     };
 
     encoder_para = {
         {"output_path", output_path},
+        {
+            "video_params", {
+                en_video_codec,
+                en_video_bitrate
+            }
+        },
+        {
+             "audio_params", {
+                 en_audio_codec,
+                 en_audio_bitrate
+             }
+        }
     };
 
 }
@@ -56,17 +83,9 @@ bool TranscoderBMF::transcode(std::string input_path, std::string output_path) {
     int scheduler_cnt = 0;
 
     auto graph = bmf::builder::Graph(bmf::builder::NormalMode);
-    // decoder init
 
-//    nlohmann::json decode_para = {
-//        {"input_path", input_path}
-//    };
     auto video = graph.Decode(bmf_sdk::JsonParam(decoder_para), "", scheduler_cnt++);
 
-    // encoder init
-//    nlohmann::json encode_para = {
-//                                  {"output_path", output_path},
-//                                  };
     graph.Encode(video["video"], video["audio"], bmf_sdk::JsonParam(encoder_para), "", scheduler_cnt++);
 
     nlohmann::json graph_para = {{"dump_graph", 1}};

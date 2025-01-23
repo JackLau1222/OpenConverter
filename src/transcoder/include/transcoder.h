7026fc29d4a0aa45a13928896626fa2f181f2567
JackLau1222 <2366536135@qq.com>
2025-01-23 17:17:27 +0800

    refactor the method of computing and sending process parameter
    fix the audio codec cannot set
    
    change the return value of send_process_parameter function

--- src/transcoder/include/transcoder.h
+++ src/transcoder/include/transcoder.h
@@ -16,6 +16,8 @@
 #ifndef TRANSCODER_H
 #define TRANSCODER_H
 
+#include <iostream>
+
 #include "../../common/include/process_parameter.h"
 #include "../../common/include/encode_parameter.h"
 #include "../../common/include/stream_context.h"
@@ -29,9 +31,56 @@ public:
 
     virtual bool transcode(std::string input_path, std::string output_path) = 0;
 
+    double compute_smooth_duration(double new_duration) {
+        if (new_duration >= min_duration_threshold) {
+            duration_history.push_back(new_duration);
+            if (duration_history.size() > max_history_size) {
+                duration_history.erase(duration_history.begin());
+            }
+        }
+        return duration_history.empty() ? 0.0 :
+                std::accumulate(duration_history.begin(), duration_history.end(), 0.0) / duration_history.size();
+    }
+
+    void send_process_parameter(int64_t frameNumber, int64_t frameTotalNumber) {
+        processNumber = frameNumber * 100 / frameTotalNumber;
+
+        processParameter->set_Process_Number(processNumber);
+
+        static auto last_encoder_call_time = std::chrono::system_clock::now();
+        auto now = std::chrono::system_clock::now();
+
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(now - last_encoder_call_time).count();
+        last_encoder_call_time = now;
+
+        double smooth_duration = compute_smooth_duration(duration);
+        if (frameNumber > 0 && frameTotalNumber > 0) {
+            remainTime = smooth_duration * (frameTotalNumber-frameNumber) / 1000;
+            processParameter->set_Time_Required(remainTime);
+        }
+
+        std::cout << "Process Number (percentage): " << processNumber << "%\t"
+                    << "Current duration (milliseconds): " << duration << "\t"
+                    << "Smoothed Duration: " << smooth_duration << " ms\t"
+                    << "Estimated Rest Time (seconds): " << remainTime << std::endl;
+    }
+
     ProcessParameter *processParameter = NULL;
 
     EncodeParameter *encodeParameter = NULL;
+
+    int64_t frameNumber = 0;
+
+    int64_t frameTotalNumber = 0;
+
+    int processNumber = 0;
+    double remainTime = 0;
+
+    std::chrono::system_clock::time_point last_encoder_call; // Track last call time
+
+    std::vector<double> duration_history;  // Store recent durations for averaging
+    static constexpr size_t max_history_size = 20;  // Limit for the number of durations tracked
+    static constexpr double min_duration_threshold = 10.0;  // Ignore durations < 10 ms
 };
 
 #endif
--- src/transcoder/include/transcoder_bmf.h
+++ src/transcoder/include/transcoder_bmf.h
@@ -32,38 +32,17 @@ public:
 
     bool transcode(std::string input_path, std::string output_path);
 
-    double compute_smooth_duration(double new_duration);
-
     bmf_sdk::CBytes decoder_callback(bmf_sdk::CBytes input);
 
     bmf_sdk::CBytes encoder_callback(bmf_sdk::CBytes input);
 
 private:
-
-    static int frameNumber;
-
-    int64_t frameTotalNumber;
-
     // encoder's parameters
     bool copyVideo;
     bool copyAudio;
 
     nlohmann::json decoder_para;
     nlohmann::json encoder_para;
-
-    // Global variable or pass-by-reference as needed
-    int frame_number_total = 0;
-    int frame_number_global = 0;
-
-    int process_number = 0;
-    double rest_time = 0;
-
-    std::chrono::system_clock::time_point last_encoder_call; // Track last call time
-    bool first_encoder_call = true; // Flag for first call
-
-    std::vector<double> duration_history;  // Store recent durations for averaging
-    static constexpr size_t max_history_size = 20;  // Limit for the number of durations tracked
-    static constexpr double min_duration_threshold = 10.0;  // Ignore durations < 10 ms
 };
 
 #endif // TRANSCODER_BMF_H

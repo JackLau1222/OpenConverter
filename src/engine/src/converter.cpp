4a890918c3df9604e40e0875a77d911b89b4a494
JackLau1222 <2366536135@qq.com>
2025-01-25 11:15:14 +0800

    feat: enable the ability to change the transcoder core while the program is running.
    modify the default option
    
    update the function name
    
    remove qdebug function
    
    update CMakeList.txt

--- src/engine/src/converter.cpp
+++ src/engine/src/converter.cpp
@@ -1,13 +1,13 @@
 #include "../include/converter.h"
 
-#if defined(USE_BMF)
+#if defined(ENABLE_BMF)
     #include "../../transcoder/include/transcoder_bmf.h"
-#elif defined(USE_FFMPEG)
+#endif
+#if defined(ENABLE_FFMPEG)
     #include "../../transcoder/include/transcoder_ffmpeg.h"
-#elif defined(USE_FFTOOL)
+#endif
+#if defined(ENABLE_FFTOOL)
     #include "../../transcoder/include/transcoder_fftool.h"
-#else
-    #error "No transcoder selected. Please define USE_BMF or USE_FFMPEG."
 #endif
 
 Converter::Converter() {}
@@ -15,15 +15,52 @@ Converter::Converter() {}
 Converter::Converter(ProcessParameter *processParamter,
                      EncodeParameter *encodeParamter)
     : processParameter(processParamter), encodeParameter(encodeParamter) {
-    #if defined(USE_BMF)
-        transcoder = new TranscoderBMF(this->processParameter, this->encodeParameter);
-    #elif defined(USE_FFMPEG)
-        transcoder = new TranscoderFFmpeg(this->processParameter, this->encodeParameter);
-    #elif defined(USE_FFTOOL)
-        transcoder = new TranscoderFFTool(this->processParameter, this->encodeParameter);
-    #endif
-    
+    // #if defined(USE_BMF)
+    //     transcoder = new TranscoderBMF(this->processParameter, this->encodeParameter);
+    // #elif defined(USE_FFMPEG)
+    //     transcoder = new TranscoderFFmpeg(this->processParameter, this->encodeParameter);
+    // #elif defined(USE_FFTOOL)
+    //     transcoder = new TranscoderFFTool(this->processParameter, this->encodeParameter);
+    // #endif
+
+    // Default transcoder
+#if defined(ENABLE_FFMPEG)
+    transcoder = new TranscoderFFmpeg(this->processParameter, this->encodeParameter);
+#endif
+
     this->encodeParameter = encodeParamter;
+}
+
+bool Converter::set_Transcoder(std::string transcoderName) {
+    if (transcoder) {
+        delete(transcoder);
+        transcoder = NULL;
+    }
+    if (transcoder == NULL) {
+        if (transcoderName == "FFMPEG") {
+#if defined(ENABLE_FFMPEG)
+            transcoder = new TranscoderFFmpeg(this->processParameter, this->encodeParameter);
+#endif
+            std::cout << "Set FFmpeg Transcoder!" << std::endl;
+        } else if (transcoderName == "BMF") {
+#if defined(ENABLE_BMF)
+            transcoder = new TranscoderBMF(this->processParameter, this->encodeParameter);
+            std::cout << "Set BMF Transcoder!" << std::endl;
+#endif
+        } else if (transcoderName == "FFTOOL") {
+#if defined(ENABLE_FFTOOL)
+            transcoder = new TranscoderFFTool(this->processParameter, this->encodeParameter);
+            std::cout << "Set FFTool Transcoder!" << std::endl;
+#endif
+        } else {
+            std::cout << "Wrong Transcoder Name!" << std::endl;
+            return false;
+        }
+    } else {
+        std::cout << "Init transcoder failed!" << std::endl;
+        return false;
+    }
+    return true;
 }
 
 void Converter::convert_Format(QString src, QString dst) {

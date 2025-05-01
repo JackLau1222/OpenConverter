420e0b95d617491b404f2d4aa8bb925aca155906
Jack Lau <jacklau1222@qq.com>
2025-05-01 15:51:27 +0800

    main: add command execute support
    command e.x.:
    ./OpenConverter.app/Contents/MacOS/OpenConverter --i /Users/jacklau/Movies/Taylor_Swift.mp4 --o /Users/jacklau/Movies/Taylor_Swift-oc-output.mp4 --acodec aac
    
    Signed-off-by: Jack Lau <jacklau1222@qq.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,40 @@
 #include "builder/include/open_converter.h"
 
 #include <QApplication>
+#include <QCommandLineParser>
 
 int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
+    
+    QCommandLineParser parser;
+    parser.setApplicationDescription("OpenConverter");
+    parser.addHelpOption();
+
+    QCommandLineOption inOpt({"i","in"},  "Input file",  "input");
+    QCommandLineOption outOpt({"o","out"}, "Output file", "output");
+    QCommandLineOption videoCodecOpt("vcodec", "Video codec", "libx264");
+    QCommandLineOption audioCodecOpt("acodec", "Audio codec", "aac");
+    QCommandLineOption presetOpt("preset", "Preset name", "preset", "medium");
+
+    parser.addOptions({ inOpt, outOpt, videoCodecOpt, audioCodecOpt, presetOpt });
+    parser.process(app);
+
+    if (parser.isSet(inOpt) || parser.isSet(outOpt)) {
+        EncodeParameter *encodeParameter = new EncodeParameter;
+        ProcessParameter *processParameter = new ProcessParameter;
+        Converter *converter = new Converter(processParameter, encodeParameter);
+        QString inFile  = parser.value(inOpt);
+        QString outFile = parser.value(outOpt);
+        encodeParameter->set_Video_Codec_Name(parser.value(videoCodecOpt).toStdString());
+        encodeParameter->set_Audio_Codec_Name(parser.value(audioCodecOpt).toStdString());
+        // Dispatch to core and exit
+        converter->convert_Format(inFile, outFile);
+        // TODO: catch the error and handle it
+        return 0;
+    }
 
     OpenConverter w;
     w.setWindowTitle("Open Converter");
     w.show();
-    return a.exec();
+    return app.exec();
 }
